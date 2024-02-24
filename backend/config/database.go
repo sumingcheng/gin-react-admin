8bcc06fbd34adc8f391763998f6204dfa43d957f
sumingcheng <cjwdyzdyplxjg@gmail.com>
2024-02-24 22:13:02 +0800

    fix: 修改配置方式

--- backend/config/database.go
+++ backend/config/database.go
@@ -16,7 +16,18 @@ func init() {
 }
 
 func ConnectDatabase() *gorm.DB {
-	dsn := viper.GetString("database_dsn")
+	// 使用viper获取配置项并拼接DSN
+	dsn := fmt.Sprintf("%s:%s@tcp(%s:%d)/%s?charset=%s&parseTime=%s&loc=%s",
+		viper.GetString("database.user"),
+		viper.GetString("database.password"),
+		viper.GetString("database.host"),
+		viper.GetInt("database.port"),
+		viper.GetString("database.name"),
+		viper.GetString("database.charset"),
+		viper.GetString("database.parseTime"),
+		viper.GetString("database.loc"),
+	)
+
 	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
 	if err != nil {
 		fmt.Println("Failed to connect to database:", err)
